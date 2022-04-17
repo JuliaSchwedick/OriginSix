8e5fa3531555c2338f7434cc7fa4aba078aa0e25
Júlia Schwedick <julia.schwedick@gmail.com>
2022-04-17 18:51:46 -0300

    Clicks do menu

--- main.js
+++ main.js
@@ -1,3 +1,21 @@
+/*abre e fecha o menu quando clicar no icone*/
+
 const nav = document.querySelector('#header nav')
 
 const toggle = document.querySelectorAll('nav .toggle')
+
+for (const element of toggle) {
+  element.addEventListener('click', function () {
+    nav.classList.toggle('show')
+  })
+}
+
+/* quando clicar em algum item do menu, esconder todo o menu */
+
+const links = document.querySelectorAll('nav ul li a')
+
+for (const link of links) {
+  link.addEventListener('click', function () {
+    nav.classList.remove('show')
+  })
+}
