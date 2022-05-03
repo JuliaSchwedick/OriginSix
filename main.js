7cb2e287476116ecbd49c2aa0c2213d0e632e45e
Júlia Schwedick <julia.schwedick@gmail.com>
2022-05-03 09:38:19 -0300

    back to top

--- main.js
+++ main.js
@@ -22,10 +22,10 @@ for (const link of links) {
 
 /*mudar o header quando der scroll*/
 
-const header = document.querySelector('header')
-const navHeight = header.offsetHeight
+function changeHeaderWhenScroll() {
+  const header = document.querySelector('header')
+  const navHeight = header.offsetHeight
 
-window.addEventListener('scroll', function () {
   if (window.scrollY >= navHeight) {
     //scroll maior que a altura do header
     header.classList.add('scroll')
@@ -33,7 +33,7 @@ window.addEventListener('scroll', function () {
     header.classList.remove('scroll')
     //menor que a altura do header
   }
-})
+}
 
 /*Testimonials carousel slider swiper*/
 
@@ -59,18 +59,26 @@ scrollReveal.reveal(
 #about .image, #about .text,
 #services header, #services .card,
 #testimonials header, #testimonials .testimonials,
-#contact .text, #contact .links
+#contact .text, #contact .links,
+footer .brand, footer .social,
 `,
   { interval: 100 }
 )
 
 /*back to top button*/
 
-const backToTopButton = document.querySelector('.back-to-top')
-window.addEventListener('scroll', function () {
+function backToTop() {
+  const backToTopButton = document.querySelector('.back-to-top')
+
   if (window.scrollY >= 560) {
     backToTopButton.classList.add('show')
   } else {
     backToTopButton.classList.remove('show')
   }
+}
+
+/*when scroll*/
+window.addEventListener('scroll', function () {
+  changeHeaderWhenScroll()
+  backToTop()
 })
